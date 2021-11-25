a154aaba7a25ed9e2e1d1aa4ae7ffaa71a1f1c4e
Pablo Pasqualino <cooliopas@users.noreply.github.com>
2021-11-25 10:52:14 -0300

    minimal

--- lib/interfaces/JSENDResponse.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type JSENDSuccess<T> = {
-    status: 'success';
-    data: T;
-};
-export declare type JSENDResponse<T> = JSENDSuccess<T>;
--- lib/schemas/JSENDResponse.d.ts
+++
@@ -1,5 +0,0 @@
-import { TSchema } from "@sinclair/typebox";
-export declare const JSENDSuccessSchema: <T extends TSchema>(type: T) => import("@sinclair/typebox").TObject<{
-    status: import("@sinclair/typebox").TLiteral<"success">;
-    data: T;
-}>;
